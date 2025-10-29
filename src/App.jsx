e0676d580b357766db59413f191d62b1abb60079
rogermarco <marcrogers90@gmail.com>
2025-10-29 18:18:00 +0000

    remove memo from buildcomponents, pointless

--- src/App.jsx
+++ src/App.jsx
@@ -82,8 +82,8 @@ function App() {
       <ImperialTech key="imperial-tech" civ={civ} />,
     ];
   }
-  const componentsLeft = useMemo(() => buildComponents(civs?.[0]), [civs]);
-  const componentsRight = useMemo(() => buildComponents(civs?.[1]), [civs]);
+  const componentsLeft = () => buildComponents(civs?.[0]);
+  const componentsRight = () => buildComponents(civs?.[1]);
 
   // Resize observer to track window size
   useEffect(() => {
