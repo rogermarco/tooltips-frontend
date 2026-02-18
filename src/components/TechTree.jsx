715565de08a2e357bf264187c13e668ecf12760a
rogermarco <marcrogers90@gmail.com>
2026-02-18 01:45:59 +0000

    south america civ update - tupi done

--- src/components/TechTree.jsx
+++ src/components/TechTree.jsx
@@ -1,12 +1,11 @@
 /* eslint-disable react/prop-types */
 import { useMemo } from 'react';
 import { buildCivTechTree } from '../lib/buildCivTechTree';
-import { useAllTechtreeData } from "../hooks/helpers";
+import { useAllTechtreeData } from '../hooks/helpers';
 import castle from '../assets/techtree/castle.webp';
 import { TechIcon } from './TechIcon.jsx';
 
 export default function TechTree({ civ }) {
-
   const { data, isLoading, error } = useAllTechtreeData();
 
   const civData = useMemo(() => {
@@ -22,35 +21,59 @@ export default function TechTree({ civ }) {
   const { techTree, uniques, uniqueTechs } = civData;
 
   return (
-    <div className="tech-tree">
+    <div className='tech-tree'>
       {/* buildings */}
-      {Object.entries(techTree).map(([building, data]) => (
-        <section key={building} className={`${building} tech-tree-section`}>
-          <img src={data.icon} alt={building} className="building-icon" />
+      {Object.entries(techTree)
+        .filter(([building]) => building !== 'castle')
+        .map(([building, data]) => (
+          <section key={building} className={`${building} tech-tree-section`}>
+            <img src={data.icon} alt={building} className='building-icon' />
 
-          {/* units */}
-          <div className="icons-grid">
-            {Object.entries(data.lines).map(([line, unit]) => (
-              <TechIcon key={line} data={unit} name={unit.current} type="unit" />
-            ))}
-            
-            {/* techs */}
-            {Object.entries(data.techs).map(([tech, tdata]) => (
-              <TechIcon key={tech} data={tdata} name={tech} type="tech" />
-            ))}
-          </div>
-        </section>
-      ))}
+            {/* units */}
+            <div className='icons-grid'>
+              {Object.entries(data.lines).map(([line, unit]) => (
+                <TechIcon
+                  key={line}
+                  data={unit}
+                  name={unit.current}
+                  type='unit'
+                />
+              ))}
+
+              {/* techs */}
+              {Object.entries(data.techs).map(([tech, tdata]) => (
+                <TechIcon key={tech} data={tdata} name={tech} type='tech' />
+              ))}
+            </div>
+          </section>
+        ))}
       {/* uniques */}
-      <section className="castle tech-tree-section">
-        <img src={castle} alt="castle" className="building-icon" />
-        <div className="icons-grid">
-          <TechIcon data={uniques.unit} name={uniques.unit.name} type="unit" />
+      <section className='castle tech-tree-section'>
+        <img src={castle} alt='castle' className='building-icon' />
+        <div className='icons-grid'>
+          <TechIcon data={uniques.unit} name={uniques.unit.name} type='unit' />
           {uniques.hero && (
-            <TechIcon data={uniques.hero} name={uniques.hero.name} type="unit" />
+            <TechIcon
+              data={uniques.hero}
+              name={uniques.hero.name}
+              type='unit'
+            />
           )}
-          <TechIcon data={uniqueTechs.castle} name={uniqueTechs.castle.title} type="tech" />
-          <TechIcon data={uniqueTechs.imperial} name={uniqueTechs.imperial.title} type="tech" />
+          <TechIcon
+            data={uniqueTechs.castle}
+            name={uniqueTechs.castle.title}
+            type='tech'
+          />
+          <TechIcon
+            data={uniqueTechs.imperial}
+            name={uniqueTechs.imperial.title}
+            type='tech'
+          />
+          <TechIcon
+            data={techTree.castle.techs.hoardings}
+            name={'Hoardings'}
+            type='tech'
+          />
         </div>
       </section>
     </div>
--- src/lib/buildCivTechTree.js
+++ src/lib/buildCivTechTree.js
@@ -1,55 +1,62 @@
-import { iconMap } from "../index.js";
+import { iconMap } from '../index.js';
 
 // These units exist in the master tech tree but are regional
 // They only appear for civs that specifically list them inside their "regionals" field.
 const regionalUnits = [
-  "eagle-scout",
-  "eagle-warrior",
-  "elite-eagle-warrior",
-  "flemish-militia",
-  "fire-lancer",
-  "elite-fire-lancer",
-  "jian-swordsman",
-  "condottiero",
-  "slinger",
-  "genitour",
-  "elite-genitour",
-  "elephant-archer",
-  "elite-elephant-archer",
-  "grenadier",
-  "xianbei-raider",
-  "xolotl-warrior",
-  "camel-scout", 
-  "camel-rider", 
-  "heavy-camel-rider", 
-  "imperial-camel-rider",
-  "steppe-lancer",
-  "elite-steppe-lancer",
-  "hei-guang-cavalry",
-  "heavy-hei-guang-cavalry",
-  "shrivamsha-rider",
-  "elite-shrivamsha-rider",
-  "battle-elephant",
-  "elite-battle-elephant",
-  "armored-elephant",
-  "siege-elephant",
-  "rocket-cart",
-  "heavy-rocket-cart",
-  "war-chariot",
-  "flaming-camel",
-  "mounted-trebuchet",
-  "traction-trebuchet",
-  "turtle-ship",
-  "elite-turtle-ship",
-  "longboat",
-  "elite-longboat",
-  "caravel",
-  "elite-caravel",
-  "dromon",
-  "lou-chuan",
-  "thirisadai",
-  "warrior-priest",
-  "missionary"
+  'eagle-scout',
+  'eagle-warrior',
+  'elite-eagle-warrior',
+  'flemish-militia',
+  'fire-lancer',
+  'elite-fire-lancer',
+  'jian-swordsman',
+  'condottiero',
+  'slinger',
+  'genitour',
+  'elite-genitour',
+  'elephant-archer',
+  'elite-elephant-archer',
+  'grenadier',
+  'xianbei-raider',
+  'xolotl-warrior',
+  'camel-scout',
+  'camel-rider',
+  'heavy-camel-rider',
+  'imperial-camel-rider',
+  'steppe-lancer',
+  'elite-steppe-lancer',
+  'hei-guang-cavalry',
+  'heavy-hei-guang-cavalry',
+  'shrivamsha-rider',
+  'elite-shrivamsha-rider',
+  'battle-elephant',
+  'elite-battle-elephant',
+  'armored-elephant',
+  'siege-elephant',
+  'rocket-cart',
+  'heavy-rocket-cart',
+  'war-chariot',
+  'flaming-camel',
+  'mounted-trebuchet',
+  'traction-trebuchet',
+  'turtle-ship',
+  'elite-turtle-ship',
+  'longboat',
+  'elite-longboat',
+  'caravel',
+  'elite-caravel',
+  'dromon',
+  'lou-chuan',
+  'thirisadai',
+  'warrior-priest',
+  'missionary',
+  'catapult-galleon',
+  'champi-scout',
+  'champi-runner',
+  'champi-warrior',
+  'elite-champi-warrior',
+  'ibirapema-warrior',
+  'elite-ibirapema',
 ];
 
 /**
@@ -58,7 +65,6 @@ const regionalUnits = [
  * @returns {object|null}
  */
 export function buildCivTechTree(civId, civStrings, uniqueStrings, techTree) {
-  
   const civ = civStrings[civId];
   const uniqueTechs = uniqueStrings[civId];
   if (!civ) {
@@ -79,97 +85,102 @@ export function buildCivTechTree(civId, civStrings, uniqueStrings, techTree) {
     regionalsUnitData.forEach((u) => civRegionalUnits.add(u));
   } else if (regionalsUnitData?.name) {
     civRegionalUnits.add(regionalsUnitData.name);
-  } else if (typeof regionalsUnitData === "string") {
+  } else if (typeof regionalsUnitData === 'string') {
     civRegionalUnits.add(regionalsUnitData);
   }
 
   // --- Begin merging ---
-  const mergedTree = Object.entries(techTree).reduce((acc, [building, data]) => {
-    const buildingData = { lines: {}, techs: {}, icon: iconMap[building]};
+  const mergedTree = Object.entries(techTree).reduce(
+    (acc, [building, data]) => {
+      const buildingData = { lines: {}, techs: {}, icon: iconMap[building] };
 
-    // ----- UNIT LINES -----
-    for (const [lineName, line] of Object.entries(data.lines || {})) {
-      // Remove fully regional lines if civ doesn't get them
-      const isRegionalLine = line.progression.every((u) =>
-        regionalUnits.includes(u)
-      );
-      if (isRegionalLine) {
-        const civHasIt = line.progression.some((u) =>
-          civRegionalUnits.has(u)
+      // ----- UNIT LINES -----
+      for (const [lineName, line] of Object.entries(data.lines || {})) {
+        // Remove fully regional lines if civ doesn't get them
+        const isRegionalLine = line.progression.every((u) =>
+          regionalUnits.includes(u),
         );
-        if (!civHasIt) continue;
+        if (isRegionalLine) {
+          const civHasIt = line.progression.some((u) =>
+            civRegionalUnits.has(u),
+          );
+          if (!civHasIt) continue;
+        }
+
+        // Clone base progression
+        let progression = [...line.progression];
+
+        // Adjust progression if civ has unique replacements (eg Savar)
+        const replacementForBuilding = replacements?.[building]?.[lineName];
+        if (replacementForBuilding) {
+          const { replace, with: replaceWith } = replacementForBuilding;
+          const idx = progression.indexOf(replace);
+          if (idx !== -1) progression.splice(idx, 1, replaceWith);
+          else progression.push(replaceWith);
+        }
+
+        // Apply civ-specific extensions (eg Imperial Skirmisher)
+        const extensionForBuilding = civ.extensions?.[building]?.[lineName];
+        if (extensionForBuilding) {
+          progression = [...progression, ...extensionForBuilding];
+        }
+
+        // Filter progression to what civ actually gets
+        const availableProgression = progression.filter(
+          (u) =>
+            !excludedTechs.has(u) &&
+            (!regionalUnits.includes(u) || civRegionalUnits.has(u)),
+        );
+
+        if (availableProgression.length === 0) continue; // skip lines civ can't train
+
+        const current = availableProgression.at(-1);
+
+        buildingData.lines[lineName] = {
+          ...line,
+          progression,
+          availableProgression,
+          current,
+          disabled: false,
+          icon: iconMap[current] || null,
+        };
       }
 
-      // Clone base progression
-      let progression = [...line.progression];
-
-      // Adjust progression if civ has unique replacements (eg Savar)
-      const replacementForBuilding = replacements?.[building]?.[lineName];
-      if (replacementForBuilding) {
-        const { replace, with: replaceWith } = replacementForBuilding;
-        const idx = progression.indexOf(replace);
-        if (idx !== -1) progression.splice(idx, 1, replaceWith);
-        else progression.push(replaceWith);
+      // ----- TECHNOLOGIES -----
+      for (const [techName, techData] of Object.entries(data.techs || {})) {
+        buildingData.techs[techName] = {
+          ...techData,
+          disabled: excludedTechs.has(techName),
+          icon: iconMap[techName] || null,
+        };
       }
 
-      // Apply civ-specific extensions (eg Imperial Skirmisher)
-      const extensionForBuilding = civ.extensions?.[building]?.[lineName];
-      if (extensionForBuilding) {
-        progression = [...progression, ...extensionForBuilding];
-      }
-
-      // Filter progression to what civ actually gets
-      const availableProgression = progression.filter(
-        (u) =>
-          !excludedTechs.has(u) &&
-          (!regionalUnits.includes(u) || civRegionalUnits.has(u))
-      );
-
-      if (availableProgression.length === 0) continue; // skip lines civ can't train
-
-      const current = availableProgression.at(-1);
-
-      buildingData.lines[lineName] = {
-        ...line,
-        progression,
-        availableProgression,
-        current,
-        disabled: false,
-        icon: iconMap[current] || null
-      };
-    }
-
-    // ----- TECHNOLOGIES -----
-    for (const [techName, techData] of Object.entries(data.techs || {})) {
-      buildingData.techs[techName] = {
-        ...techData,
-        disabled: excludedTechs.has(techName),
-        icon: iconMap[techName] || null
-      };
-    }
-
-    acc[building] = buildingData;
-    return acc;
-  }, {});
+      acc[building] = buildingData;
+      return acc;
+    },
+    {},
+  );
 
   // ----- Add icons for uniques -----
   if (uniques.unit) {
-    const key = uniques.unit.name.toLowerCase().replace(/\s+/g, "-");
+    const key = uniques.unit.name.toLowerCase().replace(/\s+/g, '-');
     uniques.unit.icon = iconMap[key] || null;
   }
   if (uniques.hero) {
-    const key = uniques.hero.name.toLowerCase().replace(/\s+/g, "-");
+    const key = uniques.hero.name.toLowerCase().replace(/\s+/g, '-');
     uniques.hero.icon = iconMap[key] || null;
   }
   if (uniqueTechs) {
-    uniqueTechs.castle.icon = iconMap["unique-tech-1"];
-    uniqueTechs.imperial.icon = iconMap["unique-tech-2"];
+    uniqueTechs.castle.icon = iconMap['unique-tech-1'];
+    uniqueTechs.imperial.icon = iconMap['unique-tech-2'];
   }
+
+  // console.log(mergedTree);
 
   return {
     civId,
     uniques,
     uniqueTechs: uniqueTechs,
-    techTree: mergedTree
+    techTree: mergedTree,
   };
 }
